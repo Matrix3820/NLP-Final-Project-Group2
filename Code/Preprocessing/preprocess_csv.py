ccc93a807c55191c9160290069ac870f60c84ed2
Anirudh Rao <157045594+Matrix3820@users.noreply.github.com>
2025-04-10 03:12:22 -0400

    Create Sample of labelled data to view in excel

--- Code/Preprocessing/preprocess_csv.py
+++ Code/Preprocessing/preprocess_csv.py
@@ -101,7 +101,8 @@ file = 'Data/recipes_data.csv'
 filepath = os.path.join(CWD, file)
 print(filepath)
 
-outfile = 'Data/recipes_data_sample_labelled.csv'
+outfile = 'Data/recipes_data_labelled.csv'
+outfile_sample = 'Data/recipes_data_sample_labelled.csv'
 
 df = pd.read_csv(filepath)
 df = df[['title', 'ingredients','directions','NER']]
@@ -110,6 +111,9 @@ labelled_df_ner = df.copy()
 label_data_ner = labelled_df_ner.apply(Label_Data_Rule_Based, axis=1, result_type='expand')
 labelled_df_ner = pd.concat([labelled_df_ner, label_data_ner], axis=1)
 
-print("Data Labelled and File Saved")
+print("Data Labelled")
 labelled_df_ner.to_csv(outfile, index=False)
+labelled_df_ner.sample(n=200000, random_state=42).to_csv(outfile_sample, index=False)
+
+print("Files Saved")
 
